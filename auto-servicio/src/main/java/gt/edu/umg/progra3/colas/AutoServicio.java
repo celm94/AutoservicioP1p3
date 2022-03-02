8de4f06cdd45b2b3da16bc30d0f00dcd041724ee
Carlos Hernandez <cralitos@gmail.com>
2022-03-01 18:27:16 -0600

    commit inicial

---
+++ auto-servicio/src/main/java/gt/edu/umg/progra3/colas/AutoServicio.java
@@ -0,0 +1,39 @@
+package gt.edu.umg.progra3.colas;
+
+public class AutoServicio {
+
+    private Cola cola;
+
+    public AutoServicio(Cola cola) {
+        this.cola = cola;
+    }
+
+    /**
+     * debe retornar el auto que esta en turno, sin sacarlo de la cola
+     * @param placa
+     * @return
+     */
+    public String ingresarAuto(String placa){
+        //Ingresar codigo aqui
+        return null;
+    }
+
+    public String proximoAuto(){
+        //ingresar codigo aqui
+        return null;
+    }
+
+    /**
+     * debe retornar el auto a entregar y sacarlo de la cola
+     * @return
+     */
+    public String entregarAuto(){
+        //ingresar codigo aqui
+        return null;
+    }
+
+    public int obtenerAutosEnCola(){
+        //ingresar codigo aqui
+        return 0;
+    }
+}
